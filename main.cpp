b27919836933ffcf0d00773de2f095f466ef1654
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-09-26 16:58:11 -0700

    Grocery List Manager

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
+//Brandan McGee
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string newItem;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        do
+        {
+                cout << "\n==GROCERY LIST MANAGER==";
+                cout << "\nEnter your choice: ";
+                cout << "\n (A)dd an item";
+                cout << "\n (Q)uit";
+                cout << "\nYour choice (A/Q): ";
+                cin >> input;
+                if (input == 'A' || input == 'a')
+                {
+                        cout << "What is the item? " << endl;
+                        cin >> newItem;
+                        list[numItems] = newItem;
+                        numItems++;
+                }
+                if (numItems > 4)
+                {
+                        cout << "You'll need a bigger list!" << endl;
+                        break;
+                }
+        } while (input != 'Q'  && input != 'q');
 
-  return 0;
+        return 0;
 }
