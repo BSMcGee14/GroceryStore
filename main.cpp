c6bd8d3a8cbf13ece32cb44b472b41879e97dfeb
BSMcGee14 <bmcgee6@mail.csuchio.edu>
2018-10-10 16:46:05 -0700

    RemovableGroceries

--- main.cpp
+++ main.cpp
@@ -10,12 +10,18 @@ int main()
         int numItems = 0;
         char input;
         string newItem;
+        
+
 
         do
         {
                 cout << "\n==GROCERY LIST MANAGER==";
                 cout << "\nEnter your choice: ";
                 cout << "\n (A)dd an item";
+                if (list.size() >  0)
+                {
+                    cout << "\n (D)elete last item";
+                }
                 cout << "\n (Q)uit";
                 cout << "\nYour choice (A/Q): ";
                 cin >> input;
@@ -25,7 +31,13 @@ int main()
                         cin >> newItem;
                         list.push_back(newItem);
                 }
-
+                if (input == 'D' || input == 'd')
+                {
+                    newItem = list.back();
+                    list.pop_back();
+                    cout << newItem  << " was deleted from the list.\n";
+                }
+                
         } while (input != 'Q'  && input != 'q');
         if (list.size() > 0)
         {
